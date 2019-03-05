4f51af762c9bd46035ad9cc41ea54f917e948f87
anagutjor <anagjordano@gmail.com>
2019-03-05 11:57:32 +0100

    add login form and basic login logic

--- index.js
+++ index.js
@@ -5,17 +5,32 @@ const io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+let numUser = 0;
+
 io.on('connection', (socket) => {
     console.log('an user connected');
-    socket.on('chat message', (msg) => {
+
+    socket.on('add user', (username) => {
+        socket.username = username;
+        ++numUser;
+        console.log(`new user ${username} added`);
+        
+        socket.emit('login', numUser);
+    })
+
+    socket.on('new message', (msg) => {
         console.log('message: ' + msg);
-        io.emit('chat message', msg);
+        io.emit('new message', {
+            username: socket.username,
+            message: msg
+        });
     })
 });
 
-app.get('/', (req, res) => {
-    res.render('/public/index.html');
-});
+// app.get('/', (req, res) => {
+//     res.render('/public/index.html');
+// });
+// ------> app.get no era necesario. El routing se hace a través de app.use
 
 http.listen(3000, (req, res) => {
     console.log('listening to port 3000');
--- public/chat.js
+++ public/chat.js
@@ -1,14 +1,51 @@
 let socket = io();
 const message = document.getElementById('m');
 const messages = document.getElementById('messages');
+const login = document.getElementById('loginPage');
+const loginForm = document.getElementById('loginForm');
+const chat = document.getElementById('chatPage');
+const input = document.getElementById('loginName');
+
+chat.style.display = 'none';
+
+const setUserName = (data) => {
+
+    let username = input.value;
+
+    if (username) {
+        login.style.display = 'none';
+        chat.style.display = 'block';
+
+        socket.emit('add user', username);
+    }
+}
+
+const addParticipant = (data) => {
+    let message = '';
+    if ( data.numUser === 1 ) {
+        message = `there's 1 participant`;
+    } else {
+        message = `there are ${data.numUser} participants`;
+    }
+}
+
+loginForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    setUserName();
+})
+
+socket.on('login', (data) => {
+    console.log(data);
+})
 
 document.getElementsByTagName('form')[0].addEventListener('submit', function(e) {
     e.preventDefault();
-    socket.emit('chat message', message.value);
+    socket.emit('new message', message.value);
     message.value = '';
     return false
 });
-socket.on('chat message', (msg) => {
+
+socket.on('new message', (msg) => {
     let newLi = document.createElement('li');
     let textNode = document.createTextNode(msg);
     newLi.appendChild(textNode);
